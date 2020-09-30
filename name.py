82d0be72bdcff79ef30074358f4574da287000a8
Shubhanshu Nishad <69467788+Shubhanshu-Nishad@users.noreply.github.com>
2020-09-30 20:31:43 +0530

    adding some important point about print() function
    Hi sir, plz merge it , I am very exited. please sir.........

--- name.py
+++ name.py
@@ -1,1 +1,5 @@
+#Useful poionts to remember about  print() function
+#The print() function prints the specified message to the screen, or other standard output device.
+#The message can be a string, or any other object, the object will be converted into a string before written to the screen.
+
 print("akHILAH")
